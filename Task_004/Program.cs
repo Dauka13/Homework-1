296366fa1aec8a455b2be21ab1cd4dd4c8a97177
Dauka <orazbekovdd13@mail.ru>
2022-08-10 23:40:32 +0600

    add task 004

---
+++ Task_004/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число чётным
+
+Console.Clear();
+int i;
+Console.Write("Введите число: ");
+i = int.Parse(Console.ReadLine());
+if (i%2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число не четное");
+}
