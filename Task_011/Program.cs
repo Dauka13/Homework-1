a9a7d6c680b0d3354a5288dd87b532226531bd1b
Dauka <orazbekovdd13@mail.ru>
2022-08-11 00:31:39 +0600

    add task 011

---
+++ Task_011/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Clear();
+int number = int.Parse(Console.ReadLine());
+string numberText = Convert.ToString(number);
+if (numberText.Length > 2)
+{
+    Console.WriteLine("третья цифра -> " + numberText[2]);
+}
+else
+{
+Console.WriteLine("-> Третьей цифры нет ");
+}
