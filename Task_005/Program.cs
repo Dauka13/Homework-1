d1300a340ab867d40e7cb5da59fd8e6a740dbcc3
Dauka <orazbekovdd13@mail.ru>
2022-08-10 23:46:19 +0600

    add task 005

---
+++ Task_005/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать числа от -N до N
+
+Console.Clear();
+Console.Write("Введите число: ");
+string writeNumber = Console.ReadLine();
+int endNumber = int.Parse(writeNumber);
+int Number = endNumber * -1;
+Console.Write($"{Number} ");
+while (Number < endNumber)
+{
+    Number++;
+    Console.Write($"{Number} ");
+}
