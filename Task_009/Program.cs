90b1795d77f6bba1fd26d8e2e7a8f17f990aa9ae
Dauka <orazbekovdd13@mail.ru>
2022-08-11 00:17:23 +0600

    add task 009

---
+++ Task_009/Program.cs
@@ -0,0 +1,13 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.Clear();
+int Number = new Random().Next(100, 1000);
+int NewNumber = 0;
+int LeftNumber()
+{
+    NewNumber = Number / 100 * 10 + Number % 10;
+    return NewNumber;
+}
+LeftNumber();
+System.Console.WriteLine();
+System.Console.WriteLine($"{Number} итог {NewNumber}");
