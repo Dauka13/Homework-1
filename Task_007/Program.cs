140910d3ea1b421516307c7083048e058a1d78e4
Dauka <orazbekovdd13@mail.ru>
2022-08-10 23:55:19 +0600

    add task 007

---
+++ Task_007/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Clear();
+string input = Console.ReadLine();
+Console.Write("{0} {1}", input, input[1]);
+Console.WriteLine(" Вторая цифра трехзначного числа:");
