cb933814dfe937bbd5c8b1975336b92a6be2a7fb
Dauka <orazbekovdd13@mail.ru>
2022-08-10 23:00:21 +0600

    add task 003

---
+++ Task_003/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти максимальное из трех чисел
+
+Console.Clear();
+int a = 33;
+int b = 6;
+int c = 17;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
