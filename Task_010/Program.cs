803f960128c447d5154bfe4daa57dd910cb3f825
Dauka <orazbekovdd13@mail.ru>
2022-08-11 00:23:51 +0600

    add task 010

---
+++ Task_010/Program.cs
@@ -0,0 +1,21 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
+
+Console.Clear();
+Console.WriteLine();
+int Number1 = new Random().Next(11, 101);
+int Number2 = new Random().Next(1, 11);
+int result = 0;
+int Multiple()
+{
+    result = Number1 % Number2;
+    return result;
+}
+Multiple();
+Console.WriteLine();
+if (result == 0) 
+    Console.WriteLine($"Число {Number1} кратно числу {Number2}");
+else
+{
+    Console.WriteLine($"Число {Number1} не кратно числу {Number2}, остаток от деления равен {Number1 % Number2}");
+}
+Console.WriteLine();
